8b305d5e1e307794c5be222716fc547e37ac26b4
John Link <john@johnlink.net>
2016-03-11 22:59:48 -0500

    Created a GUI for ssdump. This is still a work in progress.

---
+++ ssdumpGUI.Designer.cs
@@ -0,0 +1,276 @@
+﻿namespace ssdump
+{
+    partial class ssdumpGUI
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.settingsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.editHostsToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.lblHost = new System.Windows.Forms.Label();
+            this.cboHostList = new System.Windows.Forms.ComboBox();
+            this.lblUsername = new System.Windows.Forms.Label();
+            this.txtUsername = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.lblPassword = new System.Windows.Forms.Label();
+            this.ckbNoData = new System.Windows.Forms.CheckBox();
+            this.txtDatabase = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblTables = new System.Windows.Forms.Label();
+            this.txtTables = new System.Windows.Forms.TextBox();
+            this.saveFileDialog1 = new System.Windows.Forms.SaveFileDialog();
+            this.btnExecute = new System.Windows.Forms.Button();
+            this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+            this.menuStrip1.SuspendLayout();
+            this.tableLayoutPanel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem,
+            this.settingsToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(281, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "Exit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.exitToolStripMenuItem_Click);
+            // 
+            // settingsToolStripMenuItem
+            // 
+            this.settingsToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.editHostsToolStripMenuItem});
+            this.settingsToolStripMenuItem.Name = "settingsToolStripMenuItem";
+            this.settingsToolStripMenuItem.Size = new System.Drawing.Size(61, 20);
+            this.settingsToolStripMenuItem.Text = "Settings";
+            // 
+            // editHostsToolStripMenuItem
+            // 
+            this.editHostsToolStripMenuItem.Name = "editHostsToolStripMenuItem";
+            this.editHostsToolStripMenuItem.Size = new System.Drawing.Size(143, 22);
+            this.editHostsToolStripMenuItem.Text = "Edit Host List";
+            this.editHostsToolStripMenuItem.Click += new System.EventHandler(this.editHostsToolStripMenuItem_Click);
+            // 
+            // lblHost
+            // 
+            this.lblHost.AutoSize = true;
+            this.lblHost.Location = new System.Drawing.Point(3, 0);
+            this.lblHost.Name = "lblHost";
+            this.lblHost.Size = new System.Drawing.Size(32, 13);
+            this.lblHost.TabIndex = 1;
+            this.lblHost.Text = "Host:";
+            // 
+            // cboHostList
+            // 
+            this.cboHostList.FormattingEnabled = true;
+            this.cboHostList.Location = new System.Drawing.Point(95, 3);
+            this.cboHostList.Name = "cboHostList";
+            this.cboHostList.Size = new System.Drawing.Size(121, 21);
+            this.cboHostList.TabIndex = 2;
+            // 
+            // lblUsername
+            // 
+            this.lblUsername.AutoSize = true;
+            this.lblUsername.Location = new System.Drawing.Point(3, 31);
+            this.lblUsername.Name = "lblUsername";
+            this.lblUsername.Size = new System.Drawing.Size(58, 13);
+            this.lblUsername.TabIndex = 3;
+            this.lblUsername.Text = "Username:";
+            // 
+            // txtUsername
+            // 
+            this.txtUsername.Location = new System.Drawing.Point(95, 34);
+            this.txtUsername.Name = "txtUsername";
+            this.txtUsername.Size = new System.Drawing.Size(121, 20);
+            this.txtUsername.TabIndex = 4;
+            this.txtUsername.TextChanged += new System.EventHandler(this.txtUsername_TextChanged);
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(95, 65);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(121, 20);
+            this.txtPassword.TabIndex = 6;
+            this.txtPassword.TextChanged += new System.EventHandler(this.txtPassword_TextChanged);
+            // 
+            // lblPassword
+            // 
+            this.lblPassword.AutoSize = true;
+            this.lblPassword.Location = new System.Drawing.Point(3, 62);
+            this.lblPassword.Name = "lblPassword";
+            this.lblPassword.Size = new System.Drawing.Size(56, 13);
+            this.lblPassword.TabIndex = 5;
+            this.lblPassword.Text = "Password:";
+            // 
+            // ckbNoData
+            // 
+            this.ckbNoData.AutoSize = true;
+            this.ckbNoData.CheckAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.ckbNoData.Location = new System.Drawing.Point(26, 273);
+            this.ckbNoData.Name = "ckbNoData";
+            this.ckbNoData.Size = new System.Drawing.Size(138, 17);
+            this.ckbNoData.TabIndex = 8;
+            this.ckbNoData.Text = "Schema Only (No Data)";
+            this.ckbNoData.UseVisualStyleBackColor = true;
+            this.ckbNoData.CheckedChanged += new System.EventHandler(this.ckbNoData_CheckedChanged);
+            // 
+            // txtDatabase
+            // 
+            this.txtDatabase.Location = new System.Drawing.Point(95, 96);
+            this.txtDatabase.Name = "txtDatabase";
+            this.txtDatabase.Size = new System.Drawing.Size(121, 20);
+            this.txtDatabase.TabIndex = 10;
+            this.txtDatabase.TextChanged += new System.EventHandler(this.txtDatabase_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 93);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Database:";
+            // 
+            // lblTables
+            // 
+            this.lblTables.AutoSize = true;
+            this.lblTables.Location = new System.Drawing.Point(23, 162);
+            this.lblTables.Name = "lblTables";
+            this.lblTables.Size = new System.Drawing.Size(42, 13);
+            this.lblTables.TabIndex = 11;
+            this.lblTables.Text = "Tables:";
+            // 
+            // txtTables
+            // 
+            this.txtTables.Location = new System.Drawing.Point(26, 179);
+            this.txtTables.Multiline = true;
+            this.txtTables.Name = "txtTables";
+            this.txtTables.Size = new System.Drawing.Size(216, 76);
+            this.txtTables.TabIndex = 12;
+            this.txtTables.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // saveFileDialog1
+            // 
+            this.saveFileDialog1.FileOk += new System.ComponentModel.CancelEventHandler(this.saveFileDialog1_FileOk);
+            // 
+            // btnExecute
+            // 
+            this.btnExecute.Location = new System.Drawing.Point(26, 306);
+            this.btnExecute.Name = "btnExecute";
+            this.btnExecute.Size = new System.Drawing.Size(216, 23);
+            this.btnExecute.TabIndex = 15;
+            this.btnExecute.Text = "Save SQL Server Dump to a File...";
+            this.btnExecute.UseVisualStyleBackColor = true;
+            this.btnExecute.Click += new System.EventHandler(this.btnExecute_Click);
+            // 
+            // tableLayoutPanel1
+            // 
+            this.tableLayoutPanel1.ColumnCount = 2;
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 40.27778F));
+            this.tableLayoutPanel1.ColumnStyles.Add(new System.Windows.Forms.ColumnStyle(System.Windows.Forms.SizeType.Percent, 59.72222F));
+            this.tableLayoutPanel1.Controls.Add(this.txtPassword, 1, 2);
+            this.tableLayoutPanel1.Controls.Add(this.lblPassword, 0, 2);
+            this.tableLayoutPanel1.Controls.Add(this.cboHostList, 1, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblHost, 0, 0);
+            this.tableLayoutPanel1.Controls.Add(this.lblUsername, 0, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtUsername, 1, 1);
+            this.tableLayoutPanel1.Controls.Add(this.txtDatabase, 1, 3);
+            this.tableLayoutPanel1.Controls.Add(this.label1, 0, 3);
+            this.tableLayoutPanel1.Location = new System.Drawing.Point(26, 39);
+            this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+            this.tableLayoutPanel1.RowCount = 4;
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.RowStyles.Add(new System.Windows.Forms.RowStyle(System.Windows.Forms.SizeType.Percent, 25F));
+            this.tableLayoutPanel1.Size = new System.Drawing.Size(230, 124);
+            this.tableLayoutPanel1.TabIndex = 17;
+            // 
+            // ssdumpGUI
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(281, 348);
+            this.Controls.Add(this.tableLayoutPanel1);
+            this.Controls.Add(this.btnExecute);
+            this.Controls.Add(this.txtTables);
+            this.Controls.Add(this.lblTables);
+            this.Controls.Add(this.ckbNoData);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.Name = "ssdumpGUI";
+            this.Text = "ssdump (SQL Server Dump)";
+            this.Activated += new System.EventHandler(this.ssdumpGUI_Activated);
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.tableLayoutPanel1.ResumeLayout(false);
+            this.tableLayoutPanel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem settingsToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem editHostsToolStripMenuItem;
+        private System.Windows.Forms.Label lblHost;
+        private System.Windows.Forms.ComboBox cboHostList;
+        private System.Windows.Forms.Label lblUsername;
+        private System.Windows.Forms.TextBox txtUsername;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label lblPassword;
+        private System.Windows.Forms.CheckBox ckbNoData;
+        private System.Windows.Forms.TextBox txtDatabase;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblTables;
+        private System.Windows.Forms.TextBox txtTables;
+        private System.Windows.Forms.SaveFileDialog saveFileDialog1;
+        private System.Windows.Forms.Button btnExecute;
+        private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    }
+}
---
+++ ssdumpGUI.cs
@@ -0,0 +1,103 @@
+﻿using Mono.Options;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ssdump
+{
+    public partial class ssdumpGUI : Form
+    {
+        DumpProcessor processor = new DumpProcessor();
+
+        public ssdumpGUI()
+        {
+            InitializeComponent();
+            cboHostList.DataSource = Properties.Settings.Default.Hosts;
+        }
+
+        private void ssdumpGUI_Activated(object sender, System.EventArgs e)
+        {
+            cboHostList.DataSource = null;
+            cboHostList.DataSource = Properties.Settings.Default.Hosts;
+        }
+
+        private void editHostsToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            EditHostsGUI HostEditForm = new EditHostsGUI();
+            HostEditForm.StartPosition = FormStartPosition.CenterScreen;
+            HostEditForm.Show(this);
+        }
+
+        private void exitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void ckbNoData_CheckedChanged(object sender, EventArgs e)
+        {
+            processor.NoData = ckbNoData.Checked;
+        }
+
+        private void saveFileDialog1_FileOk(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void btnExecute_Click(object sender, EventArgs e)
+        {
+            Stream myStream;
+
+            saveFileDialog1.Filter = "sql files (*.sql)|*.sql|All files(*.*)|*.*";
+            saveFileDialog1.FilterIndex = 1;
+            saveFileDialog1.RestoreDirectory = true;
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                if ((myStream = saveFileDialog1.OpenFile()) != null)
+                {
+                    myStream.Close();
+                }
+            }
+        }
+
+        /// <summary>
+        /// Set Windows Authentication flag based on if there's something in the username field.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void txtUsername_TextChanged(object sender, EventArgs e)
+        {
+            processor.Username = txtUsername.Text;
+            processor.UseWindowsAuthentication = (txtUsername.TextLength == 0);
+        }
+
+        private void txtPassword_TextChanged(object sender, EventArgs e)
+        {
+            processor.Password = txtPassword.Text;
+        }
+
+        private void txtDatabase_TextChanged(object sender, EventArgs e)
+        {
+            processor.DatabaseName = txtDatabase.Text;
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            if (processor.Tables == null)
+            {
+                processor.Tables = new List<string>();
+            } else
+            {
+                processor.Tables.Clear();
+            }
+            processor.Tables.AddRange(txtTables.Lines);
+        }
+    }
+}
