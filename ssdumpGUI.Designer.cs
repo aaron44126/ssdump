721eea315ad97f371f073db347ee760af27fb1b7
John Link <john@johnlink.net>
2016-03-14 08:21:56 -0400

    Adjusted Window resize settings and how Max Packet works.

--- ssdumpGUI.Designer.cs
+++ ssdumpGUI.Designer.cs
@@ -79,7 +79,7 @@
             this.settingsToolStripMenuItem});
             this.menuStrip1.Location = new System.Drawing.Point(0, 0);
             this.menuStrip1.Name = "menuStrip1";
-            this.menuStrip1.Size = new System.Drawing.Size(273, 24);
+            this.menuStrip1.Size = new System.Drawing.Size(384, 24);
             this.menuStrip1.TabIndex = 0;
             this.menuStrip1.Text = "menuStrip1";
             // 
@@ -463,7 +463,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(273, 533);
+            this.ClientSize = new System.Drawing.Size(384, 536);
             this.Controls.Add(this.gpbConnectionSettings);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.tableLayoutPanel1);
@@ -471,8 +471,13 @@
             this.Controls.Add(this.txtTables);
             this.Controls.Add(this.lblTables);
             this.Controls.Add(this.menuStrip1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.MainMenuStrip = this.menuStrip1;
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(400, 575);
+            this.MinimumSize = new System.Drawing.Size(400, 575);
             this.Name = "ssdumpGUI";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "ssdump (SQL Server Dump)";
             this.Activated += new System.EventHandler(this.ssdumpGUI_Activated);
             this.menuStrip1.ResumeLayout(false);
